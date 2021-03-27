56e5ce435b13165151803a7738aa0524b4f4c672
booru <boll@booru.net>
2021-03-27 22:25:15 +0100

    Add factory method for robots state
    Also add docs, toString, equals and hashcode

--- src/main/java/net/booru/slidingrobots/RobotsState.java
+++ src/main/java/net/booru/slidingrobots/RobotsState.java
@@ -1,7 +1,13 @@
 package net.booru.slidingrobots;
 
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 /**
- * Represents the current state of robot positions
+ * Represents the current state of robot positions A RobotState is immutable.
  */
 public final class RobotsState {
     private final byte[] iPositions;
@@ -11,6 +17,34 @@ public final class RobotsState {
      *                       helper_robot_2_x, ..., helper_robot_n_x, helper_robot_n_x]
      */
     public RobotsState(byte[] robotPositions) {
+        iPositions = robotPositions;
+    }
+
+    public RobotsState(List<Pair<Point, Piece>> robots) {
+        final Optional<Pair<Point, Piece>> mainRobot =
+                robots.stream().filter(pair -> pair.second == Piece.main_robot).findFirst();
+        if (mainRobot.isEmpty()) {
+            throw new IllegalArgumentException("No main robot");
+        }
+
+        final List<Pair<Point, Piece>> helperRobots =
+                robots.stream().filter(pair -> pair.second == Piece.helper_robot).collect(Collectors.toList());
+
+        if (helperRobots.size() + 1 != robots.size()) {
+            throw new IllegalArgumentException("Input contains other pieces than one main robot and helper robots");
+        }
+
+        final byte[] robotPositions = new byte[robots.size() * 2];
+        robotPositions[0] = (byte) mainRobot.get().first.x;
+        robotPositions[1] = (byte) mainRobot.get().first.y;
+
+        for (int i = 0; i < helperRobots.size(); i++) {
+            final Pair<Point, Piece> pointPiecePair = helperRobots.get(i);
+            final int index = 2 * (i + 1);
+            robotPositions[index] = (byte) pointPiecePair.first.x;
+            robotPositions[index + 1] = (byte) pointPiecePair.first.y;
+        }
+
         iPositions = robotPositions;
     }
 
@@ -48,6 +82,13 @@ public final class RobotsState {
         return iPositions[2 * robotIndex + 1];
     }
 
+    /**
+     * @param x          position
+     * @param y          position
+     * @param robotIndex the robot to ignore
+     *
+     * @return true if another robot (not robotIndex) is blocking position (x,y) else false
+     */
     public boolean isOtherRobotBlocking(final int x, final int y, final int robotIndex) {
         for (int i = 0, len = getRobotCount(); i < len; i++) {
             if (i == robotIndex) {
@@ -76,5 +117,27 @@ public final class RobotsState {
             }
         }
         return -1;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(iPositions);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final RobotsState that = (RobotsState) o;
+        return Arrays.equals(iPositions, that.iPositions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(iPositions);
     }
 }
