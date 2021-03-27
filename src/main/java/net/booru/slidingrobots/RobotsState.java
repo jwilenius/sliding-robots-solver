22312542457846d9dafeb56ab0e27e00b42d9df1
booru <boll@booru.net>
2021-03-27 23:17:08 +0100

    Add solution pretty printer

--- src/main/java/net/booru/slidingrobots/RobotsState.java
+++ src/main/java/net/booru/slidingrobots/RobotsState.java
@@ -119,6 +119,57 @@ public final class RobotsState {
         return -1;
     }
 
+    /**
+     * Convert a list of RobotsState to a more easily readable format.
+     * <p>
+     * States are expected to be sequential where only one state position tuple (x,y) differs between two consecutive
+     * states, else undefined behavior.
+     *
+     * @param moves a sequence of moves where for each pair of consecutive states only one robot has moved.
+     *
+     * @return a nice string representation of the moves
+     */
+    public static String toMovementsString(final List<RobotsState> moves) {
+        final StringBuilder sb = new StringBuilder(100);
+
+        sb.append("Moves: ");
+
+        for (int i = 1; i < moves.size(); i++) {
+            final RobotsState previousState = moves.get(i - 1);
+            final RobotsState currentState = moves.get(i);
+            final int robotIndex = indexOfFirstDifference(previousState, currentState);
+
+            final String robotName = (robotIndex == 0) ? "MainRobot" : "HelperRobot-" + robotIndex;
+            sb.append(robotName)
+              .append(" -> (")
+              .append(currentState.getPositionX(robotIndex))
+              .append(", ")
+              .append(currentState.getPositionY(robotIndex))
+              .append(") ");
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Find the first robot index where the states differ.
+     *
+     * @param state1 the first state
+     * @param state2 the second state != state1
+     *
+     * @return the first robot index where the states differ.
+     */
+    private static int indexOfFirstDifference(final RobotsState state1, final RobotsState state2) {
+        for (int i = 0; i < state2.getRobotCount(); i++) {
+            if (state1.getPositionX(i) != state2.getPositionX(i) ||
+                state1.getPositionY(i) != state2.getPositionY(i)) {
+                return i;
+            }
+        }
+
+        throw new IllegalArgumentException("The two states do not differ");
+    }
+
     @Override
     public String toString() {
         return Arrays.toString(iPositions);
