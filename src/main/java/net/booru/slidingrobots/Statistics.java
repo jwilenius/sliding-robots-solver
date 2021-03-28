04e8793f32697488381d7ac2ba23300932662e48
booru <boll@booru.net>
2021-03-28 15:24:45 +0200

    Add Solution and Statistics abstractions

---
+++ src/main/java/net/booru/slidingrobots/Statistics.java
@@ -0,0 +1,34 @@
+package net.booru.slidingrobots;
+
+public class Statistics {
+    private int iStatesCreated;
+    private int iStatesVisited;
+    private int iSolutionLength;
+    private double iTime;
+
+    public void increaseStatesCreated(int by) {
+        iStatesCreated += by;
+    }
+
+    public void increaseStatesVisited(int by) {
+        iStatesVisited += by;
+    }
+
+    public void setSolutionLength(int by) {
+        iSolutionLength = by;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Statistics: \n" +
+                             "   Time (ms):       %f\n" +
+                             "   Solution length: %d\n" +
+                             "   States Created:  %d\n" +
+                             "   States Visited:  %d\n",
+                             iTime, iSolutionLength, iStatesCreated, iStatesVisited);
+    }
+
+    public void setTime(final double time) {
+        iTime = time;
+    }
+}
