202c82910014cf57b7801975c56723e145be9790
booru <boll@booru.net>
2021-03-28 15:16:00 +0200

    Support generalized end critiera in search alg

--- src/main/java/net/booru/slidingrobots/BreadthFirstSearchRecursive.java
+++ src/main/java/net/booru/slidingrobots/BreadthFirstSearchRecursive.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 public class BreadthFirstSearchRecursive {
     private final Board iBoard;
+    private final EndCriteria iMutableEndCriteria;
 
     /**
      * Keep track of the path to the end state.
@@ -35,9 +36,13 @@ public class BreadthFirstSearchRecursive {
         }
     }
 
-
-    public BreadthFirstSearchRecursive(final Board board) {
+    /**
+     * @param board              the static board that we can make moves on
+     * @param mutableEndCriteria keeps track of the end criteria during search, knows when we have reach search target.
+     */
+    public BreadthFirstSearchRecursive(final Board board, final EndCriteria mutableEndCriteria) {
         iBoard = board;
+        iMutableEndCriteria = mutableEndCriteria;
     }
 
     /**
@@ -51,7 +56,7 @@ public class BreadthFirstSearchRecursive {
             nodesToExpand.add(startNode);
             seenStates.add(startNode.getState());
 
-            final Node endNode = searchBFS(nodesToExpand, seenStates);
+            final Node endNode = searchBFS(nodesToExpand, seenStates, iMutableEndCriteria);
 
             return extractRobotStatesFromNodePath(endNode);
         } catch (NoSolutionException e) {
@@ -59,24 +64,25 @@ public class BreadthFirstSearchRecursive {
         }
     }
 
-    private LinkedList<RobotsState> extractRobotStatesFromNodePath(final Node endNode) {
-        final LinkedList<RobotsState> path = new LinkedList<>();
-        for (Node node = endNode; node != null; node = node.getPreviousNode()) {
-            path.addFirst(node.getState());
-        }
-        return path;
-    }
-
-    private Node searchBFS(final LinkedList<Node> nodesToExpand, final Set<RobotsState> seenState)
+    private Node searchBFS(final LinkedList<Node> nodesToExpand,
+                           final Set<RobotsState> seenState,
+                           final EndCriteria endCriteria)
             throws NoSolutionException {
+
         if (nodesToExpand.isEmpty()) {
             throw new NoSolutionException();
         }
 
         final Node currentNode = nodesToExpand.poll();
 
-        if (iBoard.isGoalReached(currentNode.getState())) {
+        final EndCriteria.Result result = endCriteria.checkAndUpdate(currentNode.getState());
+
+        if (result == EndCriteria.Result.FULL) {
             return currentNode;
+        } else if (result == EndCriteria.Result.PARTIAL) {
+            seenState.clear(); // todo: this may need to be optimized? profile this.
+            nodesToExpand.clear(); // todo: this may need to be optimized? profile this.
+            seenState.add(currentNode.getState());
         }
 
         final List<RobotsState> expandedStates = expandFromState(currentNode, seenState);
@@ -85,7 +91,7 @@ public class BreadthFirstSearchRecursive {
         }
         seenState.addAll(expandedStates);
 
-        return searchBFS(nodesToExpand, seenState);
+        return searchBFS(nodesToExpand, seenState, endCriteria);
     }
 
     private List<RobotsState> expandFromState(final Node currentNode, final Set<RobotsState> seenStates) {
@@ -106,6 +112,14 @@ public class BreadthFirstSearchRecursive {
         }
         // todo: apply strategy to expanded states before returning... e.g. randomize
         return expandedState;
+    }
+
+    private LinkedList<RobotsState> extractRobotStatesFromNodePath(final Node endNode) {
+        final LinkedList<RobotsState> path = new LinkedList<>();
+        for (Node node = endNode; node != null; node = node.getPreviousNode()) {
+            path.addFirst(node.getState());
+        }
+        return path;
     }
 
     private static class NoSolutionException extends Exception {
--- src/main/java/net/booru/slidingrobots/Main.java
+++ src/main/java/net/booru/slidingrobots/Main.java
@@ -6,8 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
         final String boardStr =
-                "map:helper_robot:1:4:blocker:1:6:blocker:2:0:main_robot:2:1:blocker:2:3:blocker:4:0:helper_robot:5:6" +
-                ":blocker:6:1:blocker:7:7:goal:3:0";
+                // "map:helper_robot:1:4:blocker:1:6:blocker:2:0:main_robot:2:1:blocker:2:3:blocker:4:0:helper_robot
+                // :5:6:blocker:6:1:blocker:7:7:goal:3:0";
+                "map:blocker:0:0:blocker:0:4:blocker:2:4:blocker:2:5:blocker:3:1:helper_robot:3:4:helper_robot:4:1" +
+                ":blocker:4:6:main_robot:5:5:blocker:5:7:blocker:7:0:goal:6:7";
         //final String boardStrSmall = "map_3:blocker:2:0:main_robot:2:2:goal:1:0";
 
         final Game game = Game.valueOf(boardStr);
@@ -17,9 +19,11 @@ public class Main {
         System.out.println(board.printBoard(robotsState));
 
         final Timer timer = new Timer();
-        final List<RobotsState> solution = new BreadthFirstSearchRecursive(board).run(robotsState);
+        final boolean isOneWay = false;
+        final EndCriteria mutableEndCriteria = new EndCriteria(board, isOneWay);
+        final List<RobotsState> solution = new BreadthFirstSearchRecursive(board, mutableEndCriteria).run(robotsState);
         timer.close();
-        System.out.println("Time ms: " + timer.getDurationMillis());
+        System.out.println(timer);
 
         if (solution.isEmpty()) {
             System.out.println("No solution!");
