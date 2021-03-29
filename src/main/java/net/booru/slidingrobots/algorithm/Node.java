b58cb113ea79961471b21a0df83ea2b13464fd0b
booru <boll@booru.net>
2021-03-29 23:46:38 +0200

    Add hasPreviousNode() to Node and make sure state is not null

--- src/main/java/net/booru/slidingrobots/algorithm/Node.java
+++ src/main/java/net/booru/slidingrobots/algorithm/Node.java
@@ -2,6 +2,8 @@ package net.booru.slidingrobots.algorithm;
 
 import net.booru.slidingrobots.state.RobotsState;
 
+import java.util.Objects;
+
 /**
  * Keep track of the path to the end state.
  */
@@ -10,8 +12,13 @@ public class Node {
     private final RobotsState iState;
 
     public Node(final RobotsState state, final Node previous) {
+        Objects.requireNonNull(state);
         iState = state;
         iPreviousNode = previous;
+    }
+
+    public boolean hasPreviousNode() {
+        return iPreviousNode != null;
     }
 
     public RobotsState getState() {
