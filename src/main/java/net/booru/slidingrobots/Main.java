1198f6a60ef4453c76b96b9b2b7c6d7a96b16f9e
booru <boll@booru.net>
2022-10-12 23:13:56 +0200

    Add export of solution to json and readable string

--- src/main/java/net/booru/slidingrobots/Main.java
+++ src/main/java/net/booru/slidingrobots/Main.java
@@ -71,6 +71,13 @@ public class Main {
             final SlidingRobotsSearchAlgorithm searchAlgorithm = chooseAlgorithm(algorithmType, board);
             final Solution solution = searchAlgorithm.run(robotsState);
             System.out.println(solution.toString());
+
+            int i = 0;
+            for (RobotsState s : solution.getSolutionPath()) {
+                System.out.println("Board i=" + (i++));
+                System.out.println(board.printBoard(s));
+            }
+
         } catch (NoSolutionException e) {
             System.out.println("No solution");
         }
--- src/main/java/net/booru/slidingrobots/state/RobotsStateUtil.java
+++ src/main/java/net/booru/slidingrobots/state/RobotsStateUtil.java
@@ -1,7 +1,9 @@
 package net.booru.slidingrobots.state;
 
+import com.google.gson.Gson;
 import net.booru.slidingrobots.algorithm.Node;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -86,18 +88,119 @@ public final class RobotsStateUtil {
         return sb.toString();
     }
 
+    private static class Pos {
+        public final int x;
+        public final int y;
+
+        public Pos(final int x, final int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "{x=" + x + ", y=" + y + "}";
+        }
+    }
+
+    private static class Dir {
+        public final int dx;
+        public final int dy;
+
+        public Dir(final int dx, final int dy) {
+            this.dx = dx;
+            this.dy = dy;
+        }
+
+        @Override
+        public String toString() {
+            return "{dx=" + dx + ", dy=" + dy + "}";
+        }
+    }
+
+    private static class Move {
+        public final Dir dir;
+        public final Pos pos;
+        public transient int robot; // exclude from gson JSON
+
+        public Move(final Dir dir, final Pos pos, final int robot) {
+            this.dir = dir;
+            this.pos = pos;
+            this.robot = robot;
+        }
+
+        @Override
+        public String toString() {
+            return "{Pos " + pos + " -> " + "Dir " + dir + "}";
+        }
+    }
+
+    public static String toJsonResultString(final List<RobotsState> states) {
+        /*
+         [ {
+             "dir": {"dx": 1,"dy": 0},
+             "pos": {"x": 0,"y": 6}
+           },
+           ...
+          ],
+        */
+
+        final List<Move> moves = new ArrayList<>(states.size() - 1);
+        for (int i = 1; i < states.size(); i++) {
+            final RobotsState previousState = states.get(i - 1);
+            final RobotsState currentState = states.get(i);
+            final int robotIndex = indexOfFirstDifference(previousState, currentState);
+
+            final int x = previousState.getPositionX(robotIndex);
+            final int y = previousState.getPositionY(robotIndex);
+            final Pos pos = new Pos(x, y);
+
+            final int newX = currentState.getPositionX(robotIndex);
+            final int newY = currentState.getPositionY(robotIndex);
+            final Dir dir = new Dir((int) Math.signum(newX - x), (int) Math.signum(newY - y));
+
+            final Move move = new Move(dir, pos);
+            moves.add(move);
+        }
+
+        final String listOfJson = new Gson().toJson(moves);
+        return listOfJson;
+    }
+
+    public static String toMovesResultString(final List<RobotsState> states) {
+        final List<Move> moves = new ArrayList<>(states.size() - 1);
+
+        for (int i = 1; i < states.size(); i++) {
+            final RobotsState previousState = states.get(i - 1);
+            final RobotsState currentState = states.get(i);
+            final int robotIndex = indexOfFirstDifference(previousState, currentState);
+
+            final int x = previousState.getPositionX(robotIndex);
+            final int y = previousState.getPositionY(robotIndex);
+            final Pos pos = new Pos(x, y);
+
+            final int newX = currentState.getPositionX(robotIndex);
+            final int newY = currentState.getPositionY(robotIndex);
+            final Dir dir = new Dir((int) Math.signum(newX - x), (int) Math.signum(newY - y));
+
+            final Move move = new Move(dir, pos, robotIndex);
+            moves.add(move);
+        }
+
+        return moves.toString();
+    }
+
     /**
      * Find the first robot index where the states differ.
      *
      * @param state1 the first state
      * @param state2 the second state != state1
-     *
      * @return the first robot index where the states differ.
      */
     private static int indexOfFirstDifference(final RobotsState state1, final RobotsState state2) {
         for (int i = 0; i < state2.getRobotCount(); i++) {
             if (state1.getPositionX(i) != state2.getPositionX(i) ||
-                state1.getPositionY(i) != state2.getPositionY(i)) {
+                    state1.getPositionY(i) != state2.getPositionY(i)) {
                 return i;
             }
         }
