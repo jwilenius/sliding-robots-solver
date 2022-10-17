eb703dc1b1984f2434a395aec65fa67c36e8194e
booru <boll@booru.net>
2022-10-17 17:23:57 +0200

    Add a bumps counter algorithm

---
+++ src/main/java/net/booru/slidingrobots/rank/BumpsCounter.java
@@ -0,0 +1,59 @@
+package net.booru.slidingrobots.rank;
+
+import net.booru.slidingrobots.algorithm.model.Solution;
+import net.booru.slidingrobots.common.Direction;
+import net.booru.slidingrobots.state.Board;
+import net.booru.slidingrobots.state.RobotsState;
+import net.booru.slidingrobots.state.RobotsStateUtil;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Counts the moves and the number of bumps against other robots.
+ */
+public class BumpsCounter {
+
+    public int apply(final Solution solution) {
+        final Deque<RobotsState> solutionPath = new LinkedList<>(solution.getSolutionPath());
+        RobotsState previousState = solutionPath.poll(); // discard start state
+
+        int bumps = 0;
+        while (!solutionPath.isEmpty()) {
+            final RobotsState current = solutionPath.poll();
+            bumps += countBumps(current, previousState);
+            previousState = current;
+        }
+
+        return bumps;
+    }
+
+    private int countBumps(final RobotsState state, final RobotsState previousState) {
+
+        final int robotThatMoved = RobotsStateUtil.getRobotIndexOfFirstDifference(previousState, state);
+        final Direction moveDirection = RobotsStateUtil.getRobotMovementDirection(previousState, state);
+        final int positionX = state.getPositionX(robotThatMoved);
+        final int positionY = state.getPositionY(robotThatMoved);
+
+        int bumpCount = 0;
+        for (int otherRobot = 0; otherRobot < state.getRobotCount(); otherRobot++) {
+            if (otherRobot == robotThatMoved) {
+                continue;
+            }
+
+            final int xDist = Math.abs(positionX - state.getPositionX(otherRobot));
+            final int yDist = Math.abs(positionY - state.getPositionY(otherRobot));
+            if (moveDirection == Direction.down || moveDirection == Direction.up) {
+                if (xDist == 0 && yDist == 1) {
+                    bumpCount++;
+                }
+            } else { // left/right
+                if (xDist == 1 && yDist == 0) {
+                    bumpCount++;
+                }
+            }
+        }
+
+        return bumpCount;
+    }
+}
---
+++ src/test/java/net/booru/slidingrobots/rank/BumpsCounterTest.java
@@ -0,0 +1,32 @@
+package net.booru.slidingrobots.rank;
+
+import net.booru.slidingrobots.algorithm.BreadthFirstSearchIterative;
+import net.booru.slidingrobots.algorithm.NoSolutionException;
+import net.booru.slidingrobots.algorithm.model.Solution;
+import net.booru.slidingrobots.state.Game;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BumpsCounterTest {
+
+    @Test
+    void testBumps() throws NoSolutionException {
+        final Game game = Game.valueOf2d("""
+                . . . . . . b .
+                . r . . b b . .
+                b . . b . . . .
+                h . . . . . b .
+                . . . . h . . .
+                . . . . . . . .
+                . . . . . . g .
+                b . . b . . . .
+                """);
+
+        final Solution solution = new BreadthFirstSearchIterative(game.getBoard())
+                .run(game.getInitialRobotsState(), game.getEndCriteria());
+
+        final int bumps = new BumpsCounter().apply(solution);
+        assertEquals(6, bumps);
+    }
+}
